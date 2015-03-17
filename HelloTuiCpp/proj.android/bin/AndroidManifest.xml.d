704161ab6f85d83913021c09a17169405f1f92a6
your name <email@163.com>
2015-03-17 09:01:33 +0800

    修改文件

--- HelloTuiCpp/proj.android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-F:\WorkSpace\C++WorkSpace\cocos2d-x-3.2\project\HelloTuiCpp\proj.android\bin\AndroidManifest.xml : \
-F:\WorkSpace\C++WorkSpace\cocos2d-x-3.2\project\HelloTuiCpp\proj.android\AndroidManifest.xml \
